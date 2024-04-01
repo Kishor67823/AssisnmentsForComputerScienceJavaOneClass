087d6500fbfdf423c0d19a8d43c73b1ef09d7c9b
Kishor Kharel <Badmans2343@outlook.com>
2024-03-31 22:43:11 -0600

    formatter for animal shelter program run

--- src/AnimalShelter.java
+++ src/AnimalShelter.java
@@ -1,86 +1,98 @@
-interface AdoptableAnimal{
+interface AdoptableAnimal {
     String getName();
+
     String getDescription();
+
     double getPrice();
 
 }
 
-class Cat implements AdoptableAnimal{
+class Cat implements AdoptableAnimal {
     String name;
     String description;
     double price;
 
-    public Cat(String name, String description, double price){
+    public Cat(String name, String description, double price) {
         this.name = name;
         this.description = description;
-        this.price=price;
+        this.price = price;
     }
+
     @Override
-    public String getDescription(){
+    public String getDescription() {
         return this.description;
     }
+
     @Override
-    public String getName(){
+    public String getName() {
         return this.name;
     }
+
     @Override
-    public double getPrice(){
+    public double getPrice() {
         return this.price;
     }
 
 
 }
 
-abstract class Dog implements AdoptableAnimal{
+abstract class Dog implements AdoptableAnimal {
     String name;
     String description;
     double price;
-    public Dog(String name, String description, double price){
+
+    public Dog(String name, String description, double price) {
         this.name = name;
         this.description = description;
-        this.price=price;
+        this.price = price;
     }
+
     @Override
-    public String getDescription(){
+    public String getDescription() {
         return this.description;
     }
+
     @Override
-    public String getName(){
+    public String getName() {
         return this.name;
     }
+
     @Override
-    public double getPrice(){
+    public double getPrice() {
         return this.price;
     }
 
     public abstract String getBreed();
 
 }
-class pitbull extends Dog{
-    public pitbull(String name, String description, double price){
-        super (name, description, price);
+
+class pitbull extends Dog {
+    public pitbull(String name, String description, double price) {
+        super(name, description, price);
     }
 
-    public String getBreed(){
-        String nameOfDog= "pitbull";
+    public String getBreed() {
+        String nameOfDog = "pitbull";
         return nameOfDog;
     }
 
 
 }
-class goldenRetriever extends Dog{
-    public goldenRetriever(String name, String description, double price){
-        super (name, description, price);
+
+class goldenRetriever extends Dog {
+    public goldenRetriever(String name, String description, double price) {
+        super(name, description, price);
     }
-    public String getBreed(){
-        String nameOfDog= "golden Retriever";
+
+    public String getBreed() {
+        String nameOfDog = "golden Retriever";
         return nameOfDog;
     }
 }
 
 class Main {
     public static void main(String args[]) {
-        AdoptableAnimal[] animals = new AdoptableAnimal[] {
+        AdoptableAnimal[] animals = new AdoptableAnimal[]{
                 new Cat("Billy", "He sucks", 200000),
                 new Cat("Not Billy", "Way better than billy", 2),
                 new goldenRetriever("Jimbo", "Wow!", 200),
