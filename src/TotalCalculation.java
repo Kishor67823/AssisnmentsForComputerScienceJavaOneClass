d609361b81fe920e88821fd5504e8be14cf0a970
Kishor Kharel <Badmans2343@outlook.com>
2024-04-30 22:54:55 -0600

    formatted TotalCalculation Program

--- src/TotalCalculation.java
+++ src/TotalCalculation.java
@@ -10,11 +10,11 @@ public class TotalCalculation {
         double valueThree = 42.18;
         double valueFour = .99;
         double valueFive = 12.37;
-        double subtotal =0;
+        double subtotal = 0;
         double currentValue;
         double tax = 0.053;
 
-        try(FileOutputStream fileOutputStream = new FileOutputStream(items)){
+        try (FileOutputStream fileOutputStream = new FileOutputStream(items)) {
             Scanner console = new Scanner(System.in);
             PrintStream printStream = new PrintStream(fileOutputStream);
             printStream.println(valueOne);
@@ -26,19 +26,19 @@ public class TotalCalculation {
             throw new RuntimeException(e);
         }
         Scanner in = new Scanner(items);
-        while(in.hasNextDouble()){
+        while (in.hasNextDouble()) {
             currentValue = in.nextDouble();
             subtotal = subtotal + currentValue;
 
         }
 
-        try(FileOutputStream fileOutputStream = new FileOutputStream(total)){
+        try (FileOutputStream fileOutputStream = new FileOutputStream(total)) {
             PrintStream printStream = new PrintStream(fileOutputStream);
-            printStream.printf("The sub-total is %.2f \n" , subtotal);
-            printStream.printf("The tax is %.2f \n" , (subtotal * tax));
-            double totalPlusTax = subtotal*tax;
+            printStream.printf("The sub-total is %.2f \n", subtotal);
+            printStream.printf("The tax is %.2f \n", (subtotal * tax));
+            double totalPlusTax = subtotal * tax;
             double finalTotal = totalPlusTax + subtotal;
-            printStream.printf("The total is %.2f \n" , finalTotal);
+            printStream.printf("The total is %.2f \n", finalTotal);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
